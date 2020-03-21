21f99793383760f39097151b1d4e625a8975d2e2
EliBP <52673344+EliBP@users.noreply.github.com>
2020-03-21 13:18:58 -0500

    testing from code sandbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./styles.css";
-import WebFont from 'webfontloader';
+import WebFont from "webfontloader";
 //does this work?
+//how about this?
 export default function App() {
   return (
     <div className="App">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
