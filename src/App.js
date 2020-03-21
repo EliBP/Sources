73ce1e23de48daa5a541baec8535fe01fc608a6b
EliBP <52673344+EliBP@users.noreply.github.com>
2020-03-21 12:54:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import WebFont from 'webfontloader';
+
+export default function App() {
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
