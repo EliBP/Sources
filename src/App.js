2deea5d3eb46cbfbf8c7a8f2c35b6c9dceaaa49d
EliBP <52673344+EliBP@users.noreply.github.com>
2020-03-21 14:14:33 -0500

    more branch testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 import WebFont from "webfontloader";
 //branch testing
+//more testing
 export default function App() {
   return (
     <div className="App">
